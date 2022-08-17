7120e660c271112e9a4aff9717b9d5986fa50d68
Candra <candraherdiana78@gmail.com>
2022-08-17 19:59:39 +0700

    add tailwindcss & google font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,30 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  buildModules: [
+    '@nuxtjs/google-fonts'
+  ],
+    modules: [
+      '@nuxtjs/tailwindcss',
+    ],
 
+      tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config.ts',
+        viewer: false,
+      },
+
+      googleFonts: {
+        // Download so there's no external dependencies
+        download: true,
+        // FYI: if you change any of these fonts, you have to add overwriting: true to allow the plugin
+        // to re- download
+        overwriting: true,
+        families: {
+          "Poppins": {
+            wght: [400, 500, 700],
+            ital: [400, 500, 700],
+          },
+        },
+      },
 })
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="bg-bermuda">
+    Page: foo
+  </div>
+</template>
+
+<style scoped></style>
